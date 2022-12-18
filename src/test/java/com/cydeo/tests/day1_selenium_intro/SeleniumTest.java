abdea197436511c7e8896fa5285adacdb285e031
Sergii Buchynskyi <Buchik_zt@hotmail.com>
2022-12-18 16:59:30 +0200

    First push

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) {
+
+
+
+
+        //1- Setting WebDriver
+        WebDriverManager.chromedriver().setup();
+
+        //2- Create instance of Chrome Driver
+        WebDriver driver = new ChromeDriver();
+
+        //3- Test if driver is working as expected
+        driver.get("https://www.youtube.com/");
+
+
+    }
+
+}
